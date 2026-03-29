f59e0fd428d5f650442fd5a0ccd23abf1bd05ba6
sbga <sribagtrust@gmail.com>
2026-03-30 01:11:04 +0530

    latest  update

--- backend/Server.js
+++ backend/Server.js
@@ -48,7 +48,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+ 
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
